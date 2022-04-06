dd51b75d1e78f26b004a03b110e75d7a7bd8c297
Ivan <67063050+Ivanli-RT@users.noreply.github.com>
2022-04-06 16:05:56 +0800

    git commit -m pip_upgrade master

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import subprocess
+
+def upgrade():
+    #查询需要更新的包
+    outdated_info = os.popen('pip list -o').readlines()
+    #抽取报的名称
+    package_name = [i.split()[0] for i in outdated_info[2:]]
+    print(f"可升级的库有：{package_name}")
+    for i in package_name[:-1]:
+        try:
+            print(f'开始升级库：{i}', sep='\n')
+            a = os.popen(f'pip install --upgrade {i} -i https://pypi.tuna.tsinghua.edu.cn/simple/', 'r')
+        except:
+            print(f'升级发生错误：{i}')
+
+if __name__ == '__main__':
+    #更新pip版本
+    # a = os.popen('python -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple/', 'r')
+    # print(a.read())
+    upgrade()
